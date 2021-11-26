25ac9955524fbe800cdfd21b83641c354435b5d3
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2021-11-26 20:13:11 -0300

    exercicio par e impar

---
+++ exercicios-trybe/fundamentos/bloco-4-introducao-javascript-logica-program/dia-1-introducao-primeiros-passos-javascript/exercicioPrincipal/parImpar.js
@@ -0,0 +1,15 @@
+const a = 9;
+const b = 8;
+const c = 3;
+
+if(a%2==0 || b%2==0 || c%2==0){
+    console.log(true);
+} else{
+    console.log(false);
+}
+
+if(a%2!=0 || b%2!=0 || c%2!=0){
+    console.log(true);
+} else{
+    console.log(false);
+}
