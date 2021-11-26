cae6cefd42572f1e97b2058dbbe779b3108b088d
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2021-11-26 15:13:07 -0300

    primeiro exercicio de javascript

---
+++ exercicios-trybe/fundamentos/bloco-4-introducao-javascript-logica-program/dia-1-introducao-primeiros-passos-javascript/exercicio1.js
@@ -0,0 +1,13 @@
+const myName = 'Alexandre';
+const birthCity = 'Luziânia';
+let birthYear = 2002;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = 'Brasilia';
+console.log(birthCity);
