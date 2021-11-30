8795e3f0382f707be15d48e70a9ab5d73438a729
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2021-11-30 17:55:21 -0300

    tentativa falha da piramide :(

--- exercicios-trybe/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
+++ exercicios-trybe/fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -1,5 +1,6 @@
 let n = 5;
 let aux = '';
+let espaços = '';
 
 for(let linha=1; linha<=n; linha++) {
     for(let coluna=1; coluna<=n; coluna++){
@@ -17,7 +18,6 @@ for(let i = 1; i <= n; i++) {
     aux = '';
 }
 
-let espaços;
 for(let i = 1; i <= n; i++) { 
     for(let j = n-i; j>=1; j--){
         espaços += ' ';
@@ -28,4 +28,15 @@ for(let i = 1; i <= n; i++) {
     console.log(espaços+aux); 
     aux = '';
     espaços = '';
+}
+
+for(let linha=1; linha<=n-2; linha++) {
+    for(let j = 0; j<=(n/2); j++){
+        espaços += ' ';
+    }
+    for(let coluna=1; coluna<=n; coluna++){
+        aux += '*';
+    }
+    console.log(espaços+aux); 
+    aux = '';
 }
