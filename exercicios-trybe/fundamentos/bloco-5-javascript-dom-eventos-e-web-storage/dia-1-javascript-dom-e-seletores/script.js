535125e7948873b574db1bb31a17f49eaee41f33
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2021-12-03 18:46:52 -0300

    teminando o temporizador e o exercicio principal

--- exercicios-trybe/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
+++ exercicios-trybe/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -1,11 +1,11 @@
-document.getElementsByTagName('header')[0].style.backgroundColor = 'green';
+document.getElementsByTagName('header')[0].style.backgroundColor = 'rgb(76,164,109)';
 
 for(i=0; i<document.getElementsByClassName('emergency-tasks').length; i+=1){
     document.getElementsByClassName('emergency-tasks')[i].style.backgroundColor = 'pink';
 }
 
 for(i=0; i<document.querySelectorAll('.emergency-tasks div h3').length; i+=1){
-    document.querySelectorAll('.emergency-tasks div h3')[i].style.backgroundColor = 'violet';
+    document.querySelectorAll('.emergency-tasks div h3')[i].style.backgroundColor = 'rgb(145, 4, 145)';
 }
 
 for(i=0; i<document.getElementsByClassName('emergency-tasks').length; i+=1){
@@ -16,4 +16,4 @@ for(i=0; i<document.querySelectorAll('.no-emergency-tasks div h3').length; i+=1)
     document.querySelectorAll('.no-emergency-tasks div h3')[i].style.backgroundColor = 'black';
 }
 
-document.getElementById('footer-container').style.backgroundColor = 'green';
+document.getElementById('footer-container').style.backgroundColor = 'rgb(8, 77 ,110)';
