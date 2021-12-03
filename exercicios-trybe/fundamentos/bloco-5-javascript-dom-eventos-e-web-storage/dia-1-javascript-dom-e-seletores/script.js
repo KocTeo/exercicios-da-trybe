2c45548fad40a7c5ddac1e1d68ae00ab81e1c481
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2021-12-03 15:36:08 -0300

    Administrador de tempo quase finalizado :)

---
+++ exercicios-trybe/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,19 @@
+document.getElementsByTagName('header')[0].style.backgroundColor = 'green';
+
+for(i=0; i<document.getElementsByClassName('emergency-tasks').length; i+=1){
+    document.getElementsByClassName('emergency-tasks')[i].style.backgroundColor = 'pink';
+}
+
+for(i=0; i<document.querySelectorAll('.emergency-tasks div h3').length; i+=1){
+    document.querySelectorAll('.emergency-tasks div h3')[i].style.backgroundColor = 'violet';
+}
+
+for(i=0; i<document.getElementsByClassName('emergency-tasks').length; i+=1){
+    document.getElementsByClassName('no-emergency-tasks')[i].style.backgroundColor = 'yellow';
+}
+
+for(i=0; i<document.querySelectorAll('.no-emergency-tasks div h3').length; i+=1){
+    document.querySelectorAll('.no-emergency-tasks div h3')[i].style.backgroundColor = 'black';
+}
+
+document.getElementById('footer-container').style.backgroundColor = 'green';
