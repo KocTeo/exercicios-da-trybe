6c6fbac1cc07ca50cec6161423c7622d2b532966
Alexandre Teodoreto <alekteodoreto@hotmail.com>
2021-12-06 15:30:51 -0300

    exercicios de criar e remover elementos

---
+++ exercicios-trybe/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script1.js
@@ -0,0 +1,34 @@
+document.getElementById('elementoOndeVoceEsta').parentNode.style.color = 'red';
+
+document.getElementById('primeiroFilhoDoFilho').innerText = 'fazendo o exercicio';
+
+console.log(document.getElementById('pai').firstElementChild);
+
+console.log(document.getElementById('elementoOndeVoceEsta').previousElementSibling);
+
+console.log(document.getElementById('elementoOndeVoceEsta').nextSibling);
+
+console.log(document.getElementById('elementoOndeVoceEsta').nextElementSibling);
+
+console.log(document.getElementById('pai').children[2]);
+
+let paiOndeVoceEsta = document.querySelector('#pai');
+let irmaoOndeVoceEsta = document.createElement('section');
+irmaoOndeVoceEsta.innerText = 'testanduu';
+
+paiOndeVoceEsta.appendChild(irmaoOndeVoceEsta);
+
+let OndeVoceEsta = document.querySelector('#elementoOndeVoceEsta');
+let filhoOndeVoceEsta = document.createElement('section');
+
+OndeVoceEsta.appendChild(filhoOndeVoceEsta);
+
+let primeiroFilhoDoFilho = document.querySelector('#primeiroFilhoDoFilho');
+let filhoDofilho = document.createElement('section');
+
+primeiroFilhoDoFilho.appendChild(filhoDofilho);
+
+let paiDeTudo = document.querySelector('#paiDoPai');
+let removeDoPaiDeTurdo = document.removeChild('section');
+
+console.log(document.getElementById('paiDoPai'));
